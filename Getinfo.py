43cd3669904be09e788300154e4474d73f7dbaeb
Jean-Michel "Meesh <mich0391@umn.edu>
2017-08-26 15:19:29 -0500

    Update Getinfo.py

--- Getinfo.py
+++ Getinfo.py
@@ -1,14 +1,20 @@
-"""writes the Depth, QUAL score, genotype and SNP or INDEL from a single sample vcf to std out"""
- 
- 
+#!/usr/bin/python3
+"""writes the Ref, Alt, Depth, QUAL score, genotype,  SNP or INDEL, and file basename from a single sample vcf to std out""" 
 #example use
 #python3 Getinfo.py your.vcf 
 
+""" For running this on multiple VCF files in a directory you can use a command as follows
 
+for i in $( ls *.vcf ); do
+            python3 Getinfo.py $i >> <insert_results_file_name>.txt
+        done
+"""
 import sys
 import itertools
 
 seqfile = open(sys.argv[1], "r")
+basename = sys.argv[1].split("/")[-1]
+idname = basename.split("_")[0]
 identifierlist = list()
 sequenceinfolist = list()
 
@@ -18,7 +24,10 @@ for line in seqfile:
     if line.startswith("#"):
         continue
     else:
+        #print(line)
         columns = line.split("\t")
+        REF = columns[3]
+        ALT = columns[4]
         QUAL = columns[5]
         Genotype = columns[9][0:3]
         #print(len(columns))
@@ -31,6 +40,6 @@ for line in seqfile:
             segs = columns[7].split(";")
             State = "SNP"
             Depth= segs[0][3:]
-    final = itertools.chain([Depth],[QUAL],[Genotype],[State])
+    final = itertools.chain([REF], [ALT],[Depth],[QUAL],[Genotype],[State],[idname])
     sys.stdout.write(('\t'.join(final)+'\n'))
 
