34e46ee116a33d0e34c97f1294dde7d90d7d18ce
Jean-Michel "Meesh <mich0391@umn.edu>
2017-12-28 10:27:34 -0600

    Update 50k_Filter.sh

--- 50k_Filter.sh
+++ 50k_Filter.sh
@@ -1,2 +1,4 @@
-#Use the modified 50k vcf with modified CHR field
+# Use the modified 50k vcf with modified CHR field
+# The downloaded VCF from https://soybase.org/snps/soysnp50k_wm82.a2_41317.vcf.gz has GM chromosome names
+# They need to be convereted to CHR since assembly 2 uses that nomenclature
 bedtools intersect -a Other_WPT_Study/Other_WPT_Study_Results_SNPs_Filtered_PASS.vcf -b 50k_Data.vcf
