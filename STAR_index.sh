b3b3e15e5df7f02ac65c91598ae9be89772b303a
Jean-Michel "Meesh <mich0391@umn.edu>
2018-06-24 21:50:40 -0500

    Update STAR_index.sh
    Removed my DIR paths to make it cleaner looking

--- STAR_index.sh
+++ STAR_index.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 # Command used to generate STAR index for 100bp reads
+# Make sure to downlaoad the soy reference genome (https://soybase.org/GlycineBlastPages/blast_descriptions.php)
+# Also make sure to create a DIR to store your index (it is called STAR in this case)
 STAR \
     --runThreadN 8 \
     --runMode genomeGenerate \
-    --genomeDir /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/STAR \
-    --genomeFastaFiles /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
-    --sjdbGTFfile /panfs/roc/groups/13/stuparr/shared/References/Gmax.a2.v1/annotation/Gmax_275_Wm82.a2.v1.gene_exons.gff3 \
+    --genomeDir STAR \
+    --genomeFastaFiles Gmax.a2.v1/assembly/Gmax_275_v2.0.fa \
+    --sjdbGTFfile Gmax.a2.v1/annotation/Gmax_275_Wm82.a2.v1.gene_exons.gff3 \
     --sjdbGTFtagExonParentTranscript Parent \
     --sjdbOverhang 99
